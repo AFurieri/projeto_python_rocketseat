aa8c5e4bc97f35a29f682835c671f70cc3d131a2
Arthur furieri <art.furieri@gmail.com>
2025-05-13 18:08:04 -0300

    adicionando funcionalidade base do projeto (menu interativo com uma estrutura while)

---
+++ main.py
@@ -0,0 +1,29 @@
+while True:
+        print("\nmenu do gerenciamento de listas\n")
+        print("opção 1: adicionar tarefa")
+        print("opção 2: visualizar tarefas")
+        print("opção 3: atualizar tarefa")
+        print("opção 4: completar tarefa")
+        print("opção 5: deletar tarefas completadas")
+        print("opção 6: sair") 
+
+        escolha = input("digite a opção que você deseja acionar ")
+
+        if escolha == "6":
+            break
+
+        elif escolha == "1":
+            #adicionar a função respectiva
+            
+        elif escolha == "2":
+            #adicionar a função respectiva
+            
+        elif escolha == "3":
+            #adicionar a função respectiva
+            
+        elif escolha == "4":
+            #adicionar a função respectiva
+            
+        elif escolha == "5":
+            #adicionar a função respectiva
+        
